373808009b27556602592fb9d752801ceca6f877
depthso <86912923+depthso@users.noreply.github.com>
2023-06-22 18:32:59 +0100

    By MastersMZ scripts

---
+++ MastaHub.lua
@@ -0,0 +1,7 @@
+--[[
+MastaHub by the MastersMZ scripts team.
+Made with love <3
+
+Thank you screaming bird
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MastersMZ-Scripts/MastaHub/main/Mastahub.lua"))()
