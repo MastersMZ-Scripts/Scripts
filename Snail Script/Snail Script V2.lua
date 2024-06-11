d27b102e693840df3a351be7e774aa1c3ed6144b
LordDepso <86912923+depthso@users.noreply.github.com>
2024-06-11 18:47:57 +0100

    oops

--- Snail Script/Snail Script V2.lua
+++ Snail Script/Snail Script V2.lua
@@ -23,70 +23,8 @@
 	Made by Depso - mastersmzscripts.com
 	The SNAIL Script V2
 	
-	To update the config, run the script again.
+	Please run the loader script
 ]]
-
-_G.Snail_Config = {
-	Speed = 0.4,
-	TunnelSpeed = 2,
-
-	--// Offsets
-	Offset = CFrame.new(0,-1,0),
-	TunnelOffset = CFrame.new(0,-2,0), -- This is added to the Offset
-
-	--// Control
-	Teleport = Enum.KeyCode.E,
-	Tunnel = Enum.KeyCode.Q,
-	DistanceChangesSpeed = true,
-	Distance = 5,
-	UseCameraRotaton = false, -- Old movement
-
-	--// Animations
-	RotationEffect = false,
-	Enabled = true, -- If disabled, the script will not run after death
-	DirtParticles = true,
-	Sounds = true,
-
-	--// Trail Style
-	Color = ColorSequence.new{ -- Time Position, Value
-		ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 171, 3)), 
-		ColorSequenceKeypoint.new(1.00, Color3.fromRGB(132, 255, 0))
-	},
-	Transparency = NumberSequence.new{ 
-		NumberSequenceKeypoint.new(0.00, 0.40), 
-		NumberSequenceKeypoint.new(1.00, 1.00)
-	},
-	Length = 0.3, -- 0: Disabled
-
-	--// Dirt style
-	DirtColor = ColorSequence.new{ -- Time Position, Value
-		ColorSequenceKeypoint.new(0, Color3.fromRGB(193, 135, 0)), 
-		ColorSequenceKeypoint.new(1, Color3.fromRGB(158, 84, 0))
-	},
-	DirtSize = NumberSequence.new{ 
-		NumberSequenceKeypoint.new(0, 0.2), 
-		NumberSequenceKeypoint.new(1, 0.25)
-	},
-
-	--// Sounds
-	Audios = {
-		Teleport = {
-			SoundId = 507863457
-		},
-		Tunnel = {
-			SoundId = 9114127078,
-			Looped = true,
-			PlaybackSpeed = 1.2
-		},
-	},
-
-	--// Misc (Advanced)
-	Max_Height = 15,
-	Root_Height = 4,
-}
-
-
-------------------------------
 
 if not _G.Snail_Config  then
 	return warn("[SNAIL SCRIPT] Please run the Snail script loader instead, thanks.")
